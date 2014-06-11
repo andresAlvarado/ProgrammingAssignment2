2b127aada228b841faf7bbc5549002f67c2bea60
andresAlvarado <andres.alvarado@oracle.com>
2014-06-11 11:56:17 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,92 @@
-## Put comments here that give an overall description of what your
-## functions do
+# I have created to functions as the example given. 
+# Library MASS contains the ginv function, this function
+# calculates inverse matrix for the matrix given by the user.
 
-## Write a short comment describing this function
+# Make Matrix has functions to set and get values for 
+# inverse matrix and matrix given by the user.
+# It also returns a list so that any other object is 
+# able to use such functions.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeMatrix <- function( p_matrix = matrix() )
+{
+  # Local Inverse Matrix
+  
+  l_inverseMatrix <- NULL
+  
+  #Set matrix and inverseMatrix in a different environment. 
+  
+  setMatrix <- function( c_matrix )
+  {
+    p_matrix        <<- c_matrix
+    l_inverseMatrix <<- NULL
+  }
+  
+  # Get Matrix
+  
+  getMatrix <- function()
+  {
+    p_matrix    
+  }
+  
+  # Set Inverse Matrix
+  
+  setInverseMatrix <- function(p_inverseMatrix,..)
+  {
+    l_inverseMatrix <<- p_inverseMatrix
+    
+  }
+  
+  # Get Inverse Matrix
+  
+  getInverseMatrix <- function()
+  {
+    l_inverseMatrix
+  }
+  
+  # Return a list of functions so that get and set functions 
+  # are available from other functions.
+  
+  list( mcm.setMatrix        = setMatrix
+      , mcm.getMatrix        = getMatrix
+      , mcm.setInverseMatrix = setInverseMatrix
+      , mcm.getInverseMatrix = getInverseMatrix
+  ) 
+  
+} 
 
-}
 
+# cacheSolve function will make use of MASS library and
+# ginv function, aswell as the list returned by makeMatrix
+# function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  
+  library(MASS)
+  
+  # See if an inverse matrix for matrix given already exists in an environment
+  l_inverseMatrix <- x$mcm.getInverseMatrix()
+  
+  # If so, return its inverse
+  if( ! is.null(l_inverseMatrix)   )
+  {
+    print("Getting Cached Inverse Matrix")
+    return(l_inverseMatrix)
+  }
+  
+  #Else, get inverse matrix for matrix given
+  print("Setting matrix")
+  
+  #Get matrix given
+  matrix <- x$mcm.getMatrix()
+  
+  #Get inverse matrix
+  l_inverseMatrix <- ginv(matrix)
+  
+  #Set Inverse Matrix  
+  x$mcm.setInverseMatrix(l_inverseMatrix)
+  
+  #Return Inverse Matrix
+  return(l_inverseMatrix)
+  
 }
